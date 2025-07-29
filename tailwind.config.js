09e32faa99342157ad54e9bdfd6cda2246dfe014
informio777 <ps@veribrary.com>
2025-07-29 18:33:04 +0000

    Create tailwind.config.js
    Базовая конфигурация Tailwind CSS для TALL Stack:
    - Настроен content для Laravel и TallStackUI
    - Готов для расширения кастомными цветами
    - Поддержка плагинов forms и typography

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+     e x p o r t   d e f a utlrta n{s
+f o rcmo:n t'etnrta:n s[l
+  a t e Y ('0.)/ rsecsaoluerYc(e0s./9*5*)/'*,.
+  b l a d e . p h p ' , 
+           ' . / r e s o u racneism/a*t*i/o*n.Tjism'i,n
+                                    g F u n c't.i/ovne:n d'ocru/btiacl-lbsetzaicekru(i0/.t2a1l5l,s t0a.c6k1u,i /0s.r3c5/5*,* /1*).'p,h
+                                    p ' , 
+                                           ] , 
+     t h e m e :   { 
+     } , 
+  e x t e n d :   { } , 
+                                   } , 
+     p'l1u0g0i%n's::  {[
+       ] , 
+       }                     transform: 'translateY(0) scaleY(1)',
+                               animationTimingFunction: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+       },
+       },
+       },
+       },
+       },
+
+           plugins: [
+        forms,
+               typography,
+               // TallStackUI будет автоматически подключен через composer
+           ],
+       };
